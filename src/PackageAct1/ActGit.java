284c486ca8c86b904a9f4ebdd6e0b80cf2bd1494
quimpueyo <quim.pueeyo@gmail.com>
2025-10-19 18:10:26 +0200

    Commit Inicial
    Primer commit

---
+++ src/PackageAct1/ActGit.java
@@ -0,0 +1,13 @@
+package PackageAct1;
+
+public class ActGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Esta es la línea que editaremos más tarde en las ramas 1 y 2
+		System.out.println("Linea Base del Proyecto");
+
+	}
+
+}
